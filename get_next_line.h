459fe9ea751d08bb2d47293ffa2eb4d18bde8cb6
Carlos Flores Valero <carlosfloresvalero@gmail.com>
2018-09-03 14:25:26 -0700

    .h

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cflores- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/03 14:02:49 by cflores-          #+#    #+#             */
+/*   Updated: 2018/09/03 14:06:44 by cflores-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFFSIZE = 1;
+# include <libft.h>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
