ca1d69061a2c8cb64537dcec39f24ec14d505f4a
Carlos Flores Valero <carlosfloresvalero@gmail.com>
2018-10-04 00:53:53 -0700

    ALMOST DONE

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cflores- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/04 18:19:53 by cflores-          #+#    #+#             */
-/*   Updated: 2018/10/03 23:20:38 by cflores-         ###   ########.fr       */
+/*   Updated: 2018/10/04 00:52:58 by cflores-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,90 +26,56 @@ static t_list	*get_correct_file(t_list **file, int fd)
 	}
 	tmp = ft_lstnew("\0", fd);
 	ft_lstadd(file, tmp);
-	//tmp = *file;
 	return (tmp);
-	//return (ft_lstnew(NULL, fd));
 }
 
-/*
- *get_index() Where \n is finded
- */
-int				get_index(char *ptr)
+/*void			update_content(int read_bits, char *content)
 {
-	int		i = 0;
-	char	*tmp;
-	
-	tmp = ptr;
-	while (tmp[i])
+	char *tmp = NULL;
+
+	if (read_bits < (int)ft_strlen(content))
 	{
-		if (tmp[i] == '\n')
-			return (i);
-		i += 1;
+		tmp = ft_strdup(content + (read_bits + 1));
+		ft_strdel(&content);
+		content = tmp;
 	}
-	return (i);
-}
+	else
+		ft_strclr(content);
+	if (tmp)
+		ft_strdel(&tmp);
+}*/
 
 int				get_next_line(int fd, char **line)
 {
 	static t_list	*file;
 	char			buffer[BUFF_SIZE + 1];
-	int				i;
 	int				read_bits;
 	t_list			*curr;
 	char			*tmp;
 
-
 	if (fd < 0 || line == NULL || read(fd, buffer, 0) < 0)
 		return (-1);
 	curr = get_correct_file(&file, fd);
-	tmp = NULL;
-	//curr->content = NULL;
 	while ((read_bits = read(fd, buffer, BUFF_SIZE)))
 	{
 		buffer[read_bits] = '\0';
 		tmp = curr->content;
-		//ft_strdel(&curr->content);
-		//free((void *)curr->content);
-		//curr->content = NULL;
-		//free(curr->content);
-		if (!(curr->content = ft_strjoin(tmp, buffer))) //1leaki
+		if (!(curr->content = ft_strjoin(tmp, buffer)))
 			return (-1);
 		ft_strdel(&tmp);
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
-	/* end of file
-	if (read_bits == 0)	
-		return (0);
-		*/
-	//while(1);
 	if (read_bits < BUFF_SIZE && !ft_strlen(curr->content))
-	{
-		//ft_strdel(&curr-content);
-		//free(curr);
 		return (0);
-	}
-	i = ft_copyuntil(line, curr->content, '\n');//4leak
-	//i = ge
-	//t_index(curr->content);
-	//ft_strncpy(*line, curr->content, i);
-	//
-	//printf("\n%s", *line);
-//	while(1);
-//	curr->content[i+1]
-	if (i < (int) ft_strlen(curr->content))
+	read_bits = ft_copyuntil(line, curr->content, '\n');
+	if (read_bits < (int)ft_strlen(curr->content))
 	{
-		tmp = ft_strdup(curr->content + (i + 1));
+		tmp = ft_strdup(curr->content + (read_bits + 1));
 		ft_strdel(&curr->content);
 		curr->content = tmp;
 	}
 	else
 		ft_strclr(curr->content);
-	/*
-	(i< (int)ft_strlen(curr->content))
-		? curr->content += (i + 1)
-		: ft_strclr(curr->content);
-	*/
-	//while (1);
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: cflores- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 14:02:49 by cflores-          #+#    #+#             */
-/*   Updated: 2018/10/03 23:23:18 by cflores-         ###   ########.fr       */
+/*   Updated: 2018/10/04 00:40:35 by cflores-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <fcntl.h>
 # include "libft/includes/libft.h"
-# define BUFF_SIZE 32 
+# define BUFF_SIZE 32
 
 typedef struct	s_line
 {
